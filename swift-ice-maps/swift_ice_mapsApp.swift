10a803ecd5337e2651020f4b5e5138138763ef27
Kevin Calle <kcalle012@gmail.com>
2025-10-28 19:24:33 -0400

    Initial Commit

---
+++ swift-ice-maps/swift_ice_mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ice_mapsApp.swift
+//  swift-ice-maps
+//
+//  Created by Kevin Calle on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_ice_mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
